15f67c611f83f634e21f3b3a9cdb8080126ed519
Atef Rihane <rihaneatef@gmail.com>
2020-06-01 12:33:35 +0100

    database change

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', '9avril'),
+            'database' => env('DB_DATABASE', 'journey'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', '9avril2020'),
             'unix_socket' => env('DB_SOCKET', ''),
