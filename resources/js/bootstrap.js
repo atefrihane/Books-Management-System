5ce2978f882e9f2d8171ec587f9688905877ff51
Atef Rihane <rihaneatef@gmail.com>
2020-07-08 14:59:01 +0100

    test npm

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,87 +10,87 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap');
-    require('admin-lte');
-    require('chart.js');
-    require('sweetalert');
-    require('datatables.net-bs4');
-    require('summernote');
+    // require('bootstrap');
+    // require('admin-lte');
+    // require('chart.js');
+    // require('sweetalert');
+    // require('datatables.net-bs4');
+    // require('summernote');
 
-    $(document).ready(function() {
-        $('#summernote').summernote({
-            toolbar: [
-                ['style', ['style']],
-                ['font', ['bold', 'underline', 'clear']],
-                ['fontsize', ['fontsize']], , ['fontname', ['fontname']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['view', ['fullscreen']],
-                ['insert', ['link']],
-            ],
-            height: 500,
-            focus: true
+    // $(document).ready(function() {
+    //     $('#summernote').summernote({
+    //         toolbar: [
+    //             ['style', ['style']],
+    //             ['font', ['bold', 'underline', 'clear']],
+    //             ['fontsize', ['fontsize']], , ['fontname', ['fontname']],
+    //             ['color', ['color']],
+    //             ['para', ['ul', 'ol', 'paragraph']],
+    //             ['view', ['fullscreen']],
+    //             ['insert', ['link']],
+    //         ],
+    //         height: 500,
+    //         focus: true
 
-        });
-    });
+    //     });
+    // });
     // Load the full build.
     var _ = require('lodash');
 
 
-    var pieData = {
-        labels: ["Purple", "Green", "Orange", "Yellow"],
-        datasets: [{
-            data: [20, 40, 10, 30],
-            backgroundColor: [
-                "#878BB6",
-                "#4ACAB4",
-                "#FF8153",
-                "#FFEA88"
-            ]
-        }]
-    };
+    // var pieData = {
+    //     labels: ["Purple", "Green", "Orange", "Yellow"],
+    //     datasets: [{
+    //         data: [20, 40, 10, 30],
+    //         backgroundColor: [
+    //             "#878BB6",
+    //             "#4ACAB4",
+    //             "#FF8153",
+    //             "#FFEA88"
+    //         ]
+    //     }]
+    // };
 
-    // Get the context of the canvas element we want to select
-    var ctx = document.getElementById("myChart").getContext("2d");
-    //new Chart(ctx).Pie(pieData);
-    /* New way to instantiate so that it do not thows Uncaught
-     TypeError: (intermediate value).Pie is not a function" */
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
+    // // Get the context of the canvas element we want to select
+    // var ctx = document.getElementById("myChart").getContext("2d");
+    // //new Chart(ctx).Pie(pieData);
+    // /* New way to instantiate so that it do not thows Uncaught
+    //  TypeError: (intermediate value).Pie is not a function" */
+    // var myChart = new Chart(ctx, {
+    //     type: 'bar',
+    //     data: {
+    //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //         datasets: [{
+    //             label: '# of Votes',
+    //             data: [12, 19, 3, 5, 2, 3],
+    //             backgroundColor: [
+    //                 'rgba(255, 99, 132, 0.2)',
+    //                 'rgba(54, 162, 235, 0.2)',
+    //                 'rgba(255, 206, 86, 0.2)',
+    //                 'rgba(75, 192, 192, 0.2)',
+    //                 'rgba(153, 102, 255, 0.2)',
+    //                 'rgba(255, 159, 64, 0.2)'
+    //             ],
+    //             borderColor: [
+    //                 'rgba(255, 99, 132, 1)',
+    //                 'rgba(54, 162, 235, 1)',
+    //                 'rgba(255, 206, 86, 1)',
+    //                 'rgba(75, 192, 192, 1)',
+    //                 'rgba(153, 102, 255, 1)',
+    //                 'rgba(255, 159, 64, 1)'
+    //             ],
+    //             borderWidth: 1
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             yAxes: [{
+    //                 ticks: {
+    //                     beginAtZero: true
+    //                 }
+    //             }]
+    //         }
+    //     }
+    // });
 } catch (e) {}
 
 /**
